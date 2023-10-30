cf089b0e8b68629bd6c561c1783e159a5cbaef97
eys123 <esun@olin.edu>
2023-10-30 14:42:19 -0400

    calibrate servo movement

--- camera_pan_tilt/camera_pan_tilt.ino
+++ camera_pan_tilt/camera_pan_tilt.ino
@@ -3,10 +3,10 @@
 // initialize Servo info
 Servo horServo;
 const int horPin = 8;
-int hStart = 50;
+int hStart = 0;
 int hPos = hStart;
-String horMovement;
-int horMovementNum = 0;
+String horInput;
+int horMovement = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -16,13 +16,50 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    // check serial message from raspi to see which way to move
-    horMovement = Serial.readStringUntil('\n');
-    // change horMovement to some distance for servo to move
-    horMovementNum = horMovement.toInt();
-    hPos = hPos + horMovementNum;
-    Serial.println(horMovement);
-  }
-  horServo.write(hPos);
+    horInput = Serial.readStringUntil('\n');
+    horMovement = horInput.toInt();
 
+    updatePosition(horMovement);
+    
+    // check limits
+    if (hPos > 180) {
+      hPos = 180;
+    }
+    else if (hPos < 0) {
+      hPos = 0;
+    }
+
+    horServo.write(hPos);
+    Serial.println(hPos);
+  }
+}
+
+void updatePosition(horMovement) {
+  /*
+    Updates horizontal position of the servo based on
+    the input from the Raspberry Pi camera.
+
+    Arguments:
+      horMovement: integer reporting the distance (in pixels)
+      of the center of the face from the center of the camera frame
+  */
+  if (horMovement == 0) {
+      hPos += 0;
+    } 
+  else if (abs(horMovement) > 150) {
+    if (horMovement < 0) {
+          hPos +=10;
+    }
+    else {
+      hPos -= 10;
+    }
+  }
+  else {
+    if (horMovement < 0) {
+      hPos +=5;
+    }
+    else {
+      hPos -= 5;
+    }
+  }
 }
