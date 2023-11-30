2a12cd0735781a28f9b3f79ca3dd702c94d93883
dfriishecht <dfriishecht@olin.edu>
2023-11-30 13:43:12 -0500

    Update pi detection to include tracking

--- object_detection/object_detect_v2.py
+++ object_detection/object_detect_v2.py
@@ -17,6 +17,8 @@ weightsPath = "models/coco/frozen_inference_graph.pb"
 #ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 #ser.reset_input_buffer()
 
+#Set up tracker
+tracker = cv2.legacy_TrackerMOSSE.create()
 #This is some set up values to get good results
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
 net.setInputSize(320,320)
@@ -25,8 +27,8 @@ net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
 #This is to set up what the drawn box size/colour is and the font/size/colour of the name tag and confidence label   
-def getObjects(img, thres, nms, draw=True, objects=[]):
-    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+def getObjects(frame, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(frame,confThreshold=thres,nmsThreshold=nms)
 #Below has been commented out, if you want to print each sighting of an object to the console you can uncomment below     
 #print(classIds,bbox)
     if len(objects) == 0: objects = classNames
@@ -37,39 +39,49 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
             if className == "person" or className == "backpack": 
                 objectInfo.append([box,className])
                 if (draw):
-                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                    cv2.circle(img,(int((box[0]+box[2])/2), int((box[1]+box[3])/2)), 1, (0,0,255), 1)
-                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+50,box[1]+50), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                    cv2.putText(img,str(round(confidence*100,2)),(box[0],box[1]), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.rectangle(frame,(int(box[0]), int(box[1])),(int(box[0]+box[2]), int(box[1]+box[3])),color=(0,255,0),thickness=2)
+                    cv2.circle(frame,(int(box[0]+box[2]/2), int(box[1]+box[3]/2)), 1, (0,0,255), 5)
+                    cv2.putText(frame,classNames[classId-1].upper(),(box[0]+50,box[1]+50), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(frame,str(round(confidence*100,2)),(box[0],box[1]), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
     else:
         box = [1000, 1000, 1000, 1000]
     
-    return img,objectInfo,box
+    return frame,objectInfo,box
 
 #Below determines the size of the live feed window that will be displayed on the Raspberry Pi OS
 if __name__ == "__main__":
     picam = Picamera2()
     picam.start()
     
-#Below is the never ending loop that determines what will happen when an object is identified.    
-    while True:
-        frame = picam.capture_array("main")
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+#Below is the never ending loop that determines what will happen when an object is identified.
+frame_count = 0    
+while True:
+    frame = picam.capture_array("main")
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 #Below provides a huge amount of controll. the 0.45 number is the threshold number, the 0.2 number is the nms number)
-        result, objectInfo, box = getObjects(frame,0.6,0.2)
-        box[0] -= 300
-        box[2] -= 300
-        box[1] -= 240
-        box[3] -= 240
-        x_coord = (box[0]+box[2])/2
-        y_coord = (box[1]+box[3])/2
-        print([x_coord, y_coord])
-        coords = [x_coord, y_coord]
-        #ser.write(coords.encode("utf-8"))
-        #line = ser.readline().decode("utf-8").rstrip()
-        #print(f"from arduino: {line}")
+    if frame_count % 120 == 0:
+        frame, objectInfo, b_box = getObjects(frame,0.6,0.2)
+    elif frame_count % 120 == 1:
+        track_result = tracker.init(frame, b_box)
+    else:
+        track_result, b_box = tracker.update(frame)
+        cv2.rectangle(frame,(int(b_box[0]), int(b_box[1])),(int(b_box[0]+b_box[2]), int(b_box[1]+b_box[3])),color=(0,255,0),thickness=2)
+        
+        
+        
+        
+        
+    b_box[0] -= 300
+    b_box[1] -= 300
+    x_coord = (b_box[0]+box[2]/2)
+    y_coord = (b_box[1]+b_box[3]/3)
+    print([x_coord, y_coord])
+    coords = [x_coord, y_coord]
+    #ser.write(coords.encode("utf-8"))
+    #line = ser.readline().decode("utf-8").rstrip()
+    #print(f"from arduino: {line}")
 
-        #print(objectInfo)
-        cv2.imshow("Output",frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+    #print(objectInfo)
+    cv2.imshow("Output",frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
